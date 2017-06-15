0cd707742ff5651ac851734038da0c672d0c8c11
Mateus-FMA <mateusleite512@gmail.com>
2017-06-15 17:56:28 -0300

    Changing indentation settings.

--- src/br/ufpe/cin/Main.java
+++ src/br/ufpe/cin/Main.java
@@ -1,8 +1,7 @@
 package br.ufpe.cin;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+  }
 }
