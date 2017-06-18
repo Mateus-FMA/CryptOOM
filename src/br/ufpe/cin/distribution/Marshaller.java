231133894b29788883c08f597e6706d4d83f28cc
Mateus-FMA <mateusleite512@gmail.com>
2017-06-17 21:10:55 -0300

    Implementing the Marshaller for message serialization.

---
+++ src/br/ufpe/cin/distribution/Marshaller.java
@@ -0,0 +1,30 @@
+package br.ufpe.cin.distribution;
+
+import java.io.*;
+
+/**
+ * This class implements the serialization of messages between components of a distributed system
+ * using this middleware.
+ *
+ * Created by Mateus de Freitas on 17/06/2017.
+ */
+public final class Marshaller {
+
+  // Since we don't want instances of this class and it is final (cannot declare it as abstract),
+  // we define a private constructor to prevent instantiation.
+  private Marshaller() {
+
+  }
+
+  public static byte[] marshal(Object toMarshal) throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(baos);
+    oos.writeObject(toMarshal);
+
+    return baos.toByteArray();
+  }
+
+  public static Object unmarshal(byte[] toUnmarshal) throws IOException, ClassNotFoundException {
+     return (new ObjectInputStream(new ByteArrayInputStream(toUnmarshal))).readObject();
+  }
+}
