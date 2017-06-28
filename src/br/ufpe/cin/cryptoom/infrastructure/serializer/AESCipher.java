29ece60ba9c335e2308a1d7b7e1ed99e7f89a534
Mateus-FMA <mateusleite512@gmail.com>
2017-06-27 23:56:00 -0300

    Returning to a naive implementation of the AESCipher class, since the previous one did not work properly.

--- src/br/ufpe/cin/cryptoom/infrastructure/serializer/AESCipher.java
+++ src/br/ufpe/cin/cryptoom/infrastructure/serializer/AESCipher.java
@@ -4,6 +4,7 @@ import br.ufpe.cin.cryptoom.infrastructure.serializer.exceptions.UninitializedCi
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.Serializable;
 import java.security.*;
 
@@ -17,44 +18,65 @@ import java.security.*;
  * Created by Mateus de Freitas on 19/06/2017.
  */
 public final class AESCipher implements Serializable {
-  private static final int KEY_SIZE = 128;
-  private static final String CIPHER_SETTINGS = "AES/CBC/PKCS5Padding";
+  // TODO (Mateus): check for safer implementation with the previous settings.
+  private static final String CIPHER_SETTINGS = "AES";
+  private static SecretKey key = new SecretKeySpec("AwesomeSecretKey".getBytes(), "AES");
 
-  private SecureRandom rng;
-  private IvParameterSpec iv;
-  private SecretKey key;
+  // Hide default constructor.
+  private AESCipher() {
 
-  public AESCipher() throws NoSuchAlgorithmException {
-    rng = new SecureRandom();
-
-    // Generate initialization vector.
-    byte[] byteIV = new byte[AESCipher.KEY_SIZE >> 3];
-    rng.nextBytes(byteIV);
-    iv = new IvParameterSpec(byteIV);
-
-    // Generate key.
-    KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-    keyGen.init(AESCipher.KEY_SIZE);
-    key = keyGen.generateKey();
   }
 
-  public byte[] encryptByteArray(byte[] plaintext) throws GeneralSecurityException,
-          UninitializedCipherException {
-    if (key == null || iv == null)
-      throw new UninitializedCipherException("Need to generate key and initialization vector.");
-
+  public static byte[] encryptByteArray(byte[] plaintext) throws GeneralSecurityException {
     Cipher cipher = Cipher.getInstance(AESCipher.CIPHER_SETTINGS);
-    cipher.init(Cipher.ENCRYPT_MODE, key, iv, rng);
+    cipher.init(Cipher.ENCRYPT_MODE, AESCipher.key);
     return cipher.doFinal(plaintext);
   }
 
-  public byte[] decryptByteArray(byte[] ciphertext) throws GeneralSecurityException,
-          UninitializedCipherException {
-    if (key == null || iv == null)
-      throw new UninitializedCipherException("Need to generate key and initialization vector.");
-
+  public static byte[] decryptByteArray(byte[] ciphertext) throws GeneralSecurityException {
     Cipher cipher = Cipher.getInstance(AESCipher.CIPHER_SETTINGS);
-    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+    cipher.init(Cipher.DECRYPT_MODE, key);
     return cipher.doFinal(ciphertext);
   }
+
+//  private static final int KEY_SIZE = 128;
+//  private static final String CIPHER_SETTINGS = "AES/CBC/PKCS5Padding";
+//
+//  private SecureRandom rng;
+//  private IvParameterSpec iv;
+//  private SecretKey key;
+
+//  public AESCipher() throws NoSuchAlgorithmException {
+//    rng = new SecureRandom();
+//
+//    // Generate initialization vector.
+//    byte[] byteIV = new byte[AESCipher.KEY_SIZE >> 3];
+//    rng.nextBytes(byteIV);
+//    iv = new IvParameterSpec(byteIV);
+//
+//    // Generate key.
+//    KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+//    keyGen.init(AESCipher.KEY_SIZE);
+//    key = keyGen.generateKey();
+//  }
+//
+//  public byte[] encryptByteArray(byte[] plaintext) throws GeneralSecurityException,
+//          UninitializedCipherException {
+//    if (key == null || iv == null)
+//      throw new UninitializedCipherException("Need to generate key and initialization vector.");
+//
+//    Cipher cipher = Cipher.getInstance(AESCipher.CIPHER_SETTINGS);
+//    cipher.init(Cipher.ENCRYPT_MODE, key, iv, rng);
+//    return cipher.doFinal(plaintext);
+//  }
+//
+//  public byte[] decryptByteArray(byte[] ciphertext) throws GeneralSecurityException,
+//          UninitializedCipherException {
+//    if (key == null || iv == null)
+//      throw new UninitializedCipherException("Need to generate key and initialization vector.");
+//
+//    Cipher cipher = Cipher.getInstance(AESCipher.CIPHER_SETTINGS);
+//    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+//    return cipher.doFinal(ciphertext);
+//  }
 }
